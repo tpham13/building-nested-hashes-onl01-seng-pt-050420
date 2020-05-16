3a535eeb3ac5825252f1177585252858daa21774
Thanh Pham <tpham0123@gmail.com>
2020-05-16 20:07:21 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -3,28 +3,3 @@ def first_challenge
 
 end 
 
-def first_challenge
-  epic_tragedy = {
-   :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-   },
-   :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-   }
-}
-end 
-
-
-
-
-
-
-
-
-
