e236f3ff6ba2301982d77f1d382293cf6645bf89
Thanh Pham <tpham0123@gmail.com>
2020-05-16 20:19:58 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,17 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch, 
-      :martriach,
-      :hero,
-      :hero_friends
-      }, 
-    :capulet => {
-      :patriarch, 
-      :martriach,
-      :hero,
-      :hero_friends
-      }
-
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
   
 end
