31e74949e062f6eaf06efe9eaf8f49b555be5493
Thanh Pham <tpham0123@gmail.com>
2020-05-16 20:37:51 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,12 @@
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {#your hashes here!}
+        {:name => "Benvolio",
+        :age => "17",
+        :attitude => "worried"},
+        {:name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"}
       ]
    }, 
    :capulet => {
