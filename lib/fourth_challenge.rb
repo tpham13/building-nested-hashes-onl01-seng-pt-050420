26b9740e64ae61aa920b0a8ba68cfc71af52f502
Thanh Pham <tpham0123@gmail.com>
2020-05-16 20:31:36 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,11 +1,11 @@
-def fourth_challenge
+ def fourth_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+        {#your hashes here!}
       ]
    }, 
    :capulet => {
