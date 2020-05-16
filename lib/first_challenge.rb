32b7c16997ef69faaceebc9ad7334cbdca9a09c3
Thanh Pham <tpham0123@gmail.com>
2020-05-16 19:52:59 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,5 +1,5 @@
 def first_challenge
-   epic_tragedy = {:montague => {:patriarch=>{}, :matriarch=>{}, :hero=>{}, :hero_friends[]}, :capulet => {:patriarch=>{}, :matriarch=>{}, :hero=>{}, :hero_friends[]}
+   epic_tragedy = {:montague => {:patriarch=>{}, :matriarch=>{}, :hero=>{}, :hero_friends => []}, :capulet => {:patriarch =>{}, :matriarch =>{}, :hero =>{}, :hero_friends => []}
    puts first_challange
 end
 
